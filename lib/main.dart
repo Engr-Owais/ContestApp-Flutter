807e99a26772d5f3a49929350e7858d6fa26d961
Ismailtalha <ismailtalha789@gmil.com>
2020-10-10 20:53:44 +0500

    sett

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@ import 'package:contest_app/Screens/Admin_HOme.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:splashscreen/splashscreen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -17,7 +19,18 @@ class MyApp extends StatelessWidget {
       title: 'Contest Admin',
       defaultTransition: Transition.zoom,
       theme: ThemeData(primarySwatch: Colors.amber),
-      home: AdminHome(),
-    );
+      home: SplashScreen(
+            seconds: 4,
+            navigateAfterSeconds: AdminHome(),
+            title: new Text('Welcome To Contestor',
+                style: GoogleFonts.abel(
+                    fontWeight: FontWeight.bold, fontSize: 20)),
+            image: new Image(
+              image: AssetImage("assets/buffersplash.png"),
+            ),
+            backgroundColor: Colors.amber,
+            styleTextUnderTheLoader: new TextStyle(),
+            photoSize: 100,
+            loaderColor: Colors.red));
   }
 }
